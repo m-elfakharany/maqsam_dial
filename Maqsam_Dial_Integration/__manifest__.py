3458586316d886bc391a3b0bb1209791f211a365
mohamed essam <mohamed_201900705@fci.helwan.edu.eg>
2024-03-25 01:22:55 +0200

    Updated manifest file for Maqsam Dial Integration

--- Maqsam_Dial_Integration/__manifest__.py
+++ Maqsam_Dial_Integration/__manifest__.py
@@ -6,9 +6,11 @@
     'category': 'Custom',
     'summary': 'Integrate Maqsam dialing with Odoo Contacts',
     'depends': ['base', 'contacts'],
+    'license': 'LGPL-3',
     'data': [
         'views/res_partner_views.xml',
     ],
     'installable': True,
     'auto_install': False,
+    'images': ['static/description/logo.png'],
 }
